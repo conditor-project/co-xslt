5bd2f14f6666b9e7ec86a613533464e3460fe7af
Rémy MEJA <remymeja@free.fr>
2018-10-05 15:57:03 +0200

    update code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const fse = require('fs-extra');
 const path = require('path');
 const Computron = require('computron');
 const { getStylesheetFrom } = require('tei-conditor');
@@ -8,20 +8,31 @@ Promise.promisifyAll(Computron.prototype);
 
 const coXslt = {};
 coXslt.doTheJob = function (docObject, next) {
-  if (docObject.hasOwnProperty('originDocPath')) return next(new Error('no originDocPath key founded'));
+  if (!docObject.hasOwnProperty('originDocPath')) return next(new Error('no originDocPath key founded'));
+  const filename = path.basename(docObject.originDocPath, '.xml');
+  const directory = path.dirname(docObject.originDocPath);
+  const teiDocDirectory = (directory[0] === '/') ? directory : path.join(__dirname, directory);
+  const teiDocPath = path.join(teiDocDirectory, `${filename}.tei`);
   const transformer = new Computron();
-  getStylesheetFrom(docObject.source).then(stylesheets => {
-    if (stylesheets.length === 0) return Promise.reject(new Error(`no stylesheet founded for ${docObject.source}`));
-    if (stylesheets.length > 1) return Promise.reject(new Error(`more than one stylesheet founded for ${docObject.source}`));
-    const stylesheet = stylesheets.pop();
-    return transformer.loadStylesheetAsync(stylesheet.path);
-  }).then(() => transformer.applyAsync(docObject.sourcePath))
-    .then(xmlTei => {
-      const name = path.basename(docObject.originDocPath);
-      docObject.teiDocPath = path.join(docObject.corpusRoot, 'tei', name);
-      return fs.writeFileAsync(docObject.teiDocPath, xmlTei);
+  fse.ensureDir(teiDocDirectory)
+    .then(() => getStylesheetFrom(docObject.source))
+    .then(stylesheets => {
+      if (stylesheets.length === 0) return Promise.reject(new Error(`no stylesheet founded for ${docObject.source}`));
+      // FIXME : waiting a single xsl stylesheet for pubmed
+      // if (stylesheets.length > 1) return Promise.reject(new Error(`more than one stylesheet founded for ${docObject.source}`));
+      // const stylesheet = stylesheets.pop();
+      const stylesheet = stylesheets[0];
+      return transformer.loadStylesheetAsync(stylesheet.path);
     })
-    .then(() => next(null, docObject))
+    .then(() => {
+      const originDocPath = (docObject.originDocPath[0] === '/') ? docObject.originDocPath : path.join(__dirname, docObject.originDocPath);
+      return transformer.applyAsync(originDocPath);
+    })
+    .then(xmlTei => fse.writeFile(teiDocPath, xmlTei))
+    .then(() => {
+      docObject.teiDocPath = teiDocPath;
+      next(null, docObject);
+    })
     .catch(next);
 };
 

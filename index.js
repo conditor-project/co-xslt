644aa5796deb5ea9517a0ef03a3d353fae971063
niederlec <claude.niederlender@gmail.com>
2021-10-26 10:22:32 +0200

    feat: replace computron by saxon-js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
-const Promise = require('bluebird');
 const fse = require('fs-extra');
 const path = require('path');
-const Computron = require('computron');
-const { getStylesheetFrom } = require('tei-conditor');
-Promise.promisifyAll(Computron.prototype);
+const SaxonJS = require('saxon-js');
+const { getSefFrom } = require('tei-conditor');
 
 const coXslt = {};
 coXslt.doTheJob = function (docObject, next) {
@@ -13,24 +11,26 @@ coXslt.doTheJob = function (docObject, next) {
   const directory = path.dirname(docObject.originDocPath);
   const teiDocDirectory = (directory[0] === '/') ? directory : path.join(__dirname, directory);
   const teiDocPath = path.join(teiDocDirectory, `${filename}.tei`);
-  const transformer = new Computron();
   fse.ensureDir(teiDocDirectory)
-    .then(() => getStylesheetFrom(docObject.source))
+    .then(() => getSefFrom(docObject.source))
     .then(stylesheets => {
       if (stylesheets.length === 0) return Promise.reject(new Error(`no stylesheet founded for ${docObject.source}`));
       // FIXME : waiting a single xsl stylesheet for pubmed
       // if (stylesheets.length > 1) return Promise.reject(new Error(`more than one stylesheet founded for ${docObject.source}`));
       // const stylesheet = stylesheets.pop();
       const stylesheet = stylesheets[0];
-      return transformer.loadStylesheetAsync(stylesheet.path);
-    })
-    .then(() => {
       const originDocPath = (docObject.originDocPath[0] === '/') ? docObject.originDocPath : path.join(__dirname, docObject.originDocPath);
       let conf = typeof coXslt.config !== "undefined" ? coXslt.config : {};
       if (typeof conf.today === "undefined") conf.today = today();
-      return transformer.applyAsync(originDocPath, conf);
+      
+      SaxonJS.transform({
+        stylesheetFileName: stylesheet,
+        sourceFileName: originDocPath,
+        destination: "serialized"
+      }, "async");
+    }).then(output => {
+        fse.writeFile(teiDocPath, output.principalResult);
     })
-    .then(xmlTei => fse.writeFile(teiDocPath, xmlTei))
     .then(() => {
       docObject.path = teiDocPath;
       docObject.teiDocPath = teiDocPath;
